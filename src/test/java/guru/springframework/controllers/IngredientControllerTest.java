38cb09101e0d889d9b8bf4850630bf719175fc44
David Spencer <david.spencer3@tesco.com>
2021-11-16 11:21:36 +0000

    IngredientControllerTest fix

--- src/test/java/guru/springframework/controllers/IngredientControllerTest.java
+++ src/test/java/guru/springframework/controllers/IngredientControllerTest.java
@@ -39,7 +39,7 @@ public class IngredientControllerTest {
 
         mockMvc.perform(get("/recipe/1/ingredients"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("/recipe/ingredient/list"))
+                .andExpect(view().name("recipe/ingredient/list"))
                 .andExpect(model().attributeExists("recipe"));
 
         verify(recipeService, times(1)).findCommandById(anyLong());
