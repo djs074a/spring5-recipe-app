aaef88da4a725ccb6d804746154aeb52eeb6b0fd
David Spencer <david.spencer3@tesco.com>
2021-11-30 15:10:31 +0000

    Added number format exception

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -49,12 +49,21 @@ public class RecipeControllerTest {
     }
 
     @Test
-    public void testRecipeNotFound() throws Exception{
+    public void testGetRecipeNotFound() throws Exception{
 
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
+    }
+
+    @Test
+    public void testGetRecipeNumberFormat() throws Exception{
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 
     @Test
